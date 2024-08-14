2eee13a641c4705af4731078002bb1b22eb7190e
Haaris Sayyed <haaris7200@gmail.com>
2024-08-14 14:36:45 +0530

    Added remaining integration tests

--- src/test/java/com/api/redis/redis_demo/RedisDemoApplicationTests.java
+++ src/test/java/com/api/redis/redis_demo/RedisDemoApplicationTests.java
@@ -1,12 +1,18 @@
 package com.api.redis.redis_demo;
 
+import com.api.redis.redis_demo.dao.UserRepository;
+import com.api.redis.redis_demo.model.User;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+
+import static org.hamcrest.Matchers.equalTo;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,10 +21,15 @@ class RedisDemoApplicationTests {
 	@LocalServerPort
 	private Integer port;
 
-	@BeforeEach
-	void setup(){
+	@Autowired
+	private UserRepository userRepository;
+
+    @BeforeEach
+	void setup() throws Exception {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = port;
+		userRepository.save(new User("101","John","London","England"));
+		userRepository.save(new User("102","Harry","Brrokyln","USA"));
 	}
 
 	@Test
@@ -37,9 +48,62 @@ class RedisDemoApplicationTests {
 				.post("/api/users")
 				.then()
 				.statusCode(200)
-				.body("name", Matchers.equalTo("Tony Stark"))
-				.body("city", Matchers.equalTo("London"))
-				.body("country", Matchers.equalTo("England"));
+				.body("name", equalTo("Tony Stark"))
+				.body("city", equalTo("London"))
+				.body("country", equalTo("England"));
 	}
+
+    @Test
+    void shouldReturnUser(){
+        RestAssured.given()
+				.when()
+				.get("/api/users/{id}",101)
+				.then()
+				.statusCode(200)
+				.body("name", equalTo("John"))
+				.body("city", equalTo("London"))
+				.body("country", equalTo("England"));
+    }
+
+	@Test
+	void shouldReturnAllUsers(){
+		RestAssured.given()
+				.when()
+				.get("/api/users")
+				.then()
+				.statusCode(200)
+				.body("users",Matchers.hasSize(2));
+	}
+
+    @Test
+	void shouldUpdateUser(){
+		String updatedUser = """
+				{
+				     "name": "Harvard Stark",
+				     "city": "London",
+				     "country": "England"
+				 }
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(updatedUser)
+				.when()
+				.put("/api/users/{id}", "101")
+				.then()
+				.statusCode(200)
+				.contentType(ContentType.JSON)
+				.body("name", equalTo("Harvard Stark"));
+	}
+
+    @Test
+    void shouldDeleteUser(){
+        RestAssured.given()
+                .when()
+                .delete("/api/users/{id}", "101")
+                .then()
+                .statusCode(200)
+                .body(Matchers.equalTo("User deleted successfully"));;
+    }
 
 }
